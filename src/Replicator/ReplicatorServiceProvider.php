9ad0aebe6c9bd307845f88ecf5848ac7defc2df8
Roberto Griel Filho <roberto@griel.com.br>
2024-11-27 09:45:19 -0300

    Movendo migration para o diretório apropriado

--- src/Replicator/ReplicatorServiceProvider.php
+++ src/Replicator/ReplicatorServiceProvider.php
@@ -9,7 +9,7 @@ class ReplicatorServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../../migrations/');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations/');
     }
 
     public function register(): void
