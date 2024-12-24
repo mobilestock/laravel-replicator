fe248d59d11166ac4a778d10b73e360058cc0529
felprangel <feliperangel897@gmail.com>
2024-12-24 17:45:25 -0300

    altera nome da classe

--- src/Replicator/Console/Commands/MakeInterceptor.php
+++ src/Replicator/Console/Commands/MakeInterceptor.php
@@ -4,7 +4,7 @@ namespace MobileStock\LaravelReplicator\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class StartReplicationCommand extends Command
+class MakeInterceptor extends Command
 {
     protected $signature = 'make:replicator-interceptor';
     protected $description = 'Create a new interceptor for laravel replicator ';
