5dddea15ce7fc44e3a70a03737b4a0673e7b1ae0
felprangel <feliperangel897@gmail.com>
2024-12-27 12:43:36 -0300

    INDENTAÇÃO

--- src/Replicator/Console/Commands/MakeInterceptor.php
+++ src/Replicator/Console/Commands/MakeInterceptor.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 class MakeInterceptor extends Command
 {
     protected $signature = 'make:replicator-interceptor {name}';
-    protected $description = 'Create a new interceptor for laravel replicator ';
+    protected $description = 'Create a new interceptor for laravel replicator';
 
     public function handle(): void
     {
